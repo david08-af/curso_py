61509e60e5607f24421f2324e89016e8da81e91b
david08-af <leonardodavidvillalba294@gmail.com>
2025-05-24 13:38:21 +0000

    agregando ejercicio uno

---
+++ 00-mru/mru-data.py
@@ -0,0 +1,4 @@
+velocidad = 1
+tiempo = 4
+distancia = velocidad * tiempo
+print(distancia)
---
+++ 00-mru/mru-input.py
@@ -0,0 +1,4 @@
+velocidad = input("velocidadd del movimiento: ")
+tiempo = input("el tiempo: ")
+distancia = float(velocidad) * float(tiempo)
+print(distancia)
